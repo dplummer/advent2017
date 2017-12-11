841f71a5fdffd5f19537a42c8371384ad4b4f70b
Donald Plummer <dplummer@avvo.com>
2017-12-10 22:12:46 -0800

    Day 11 part 2

--- day11-part2.exs
+++ day11-part2.exs
@@ -10,8 +10,7 @@ defmodule Puzzle do
     input
     |> String.trim_trailing
     |> String.split(",")
-    |> walk_steps({0, 0, 0})
-    |> count_steps(0)
+    |> walk_steps({0, 0, 0}, 0)
   end
 
   def count_steps({0, 0, 0}, count), do: count
@@ -23,17 +22,20 @@ defmodule Puzzle do
   def count_steps({x, y, 0}, count) when x < 0, do: count + y
   def count_steps({x, y, z}, count) do
     cond do
-      x > y and y > z -> {x - 1, y + 1, z}
-      y > x and x > z -> {x + 1, y - 1, z}
-      true -> {x + 1, y, z - 1}
+      x >= y and y >= z -> {x - 1, y, z + 1}
+      x >= z and z >= y -> {x - 1, y + 1, z}
+      y >= x and x >= z -> {x, y - 1, z + 1}
+      y >= z and z >= x -> {x + 1, y - 1, z}
+      z >= x and x >= y -> {x, y + 1, z - 1}
+      z >= y and y >= x -> {x + 1, y, z - 1}
     end
     |> count_steps(count + 1)
   end
 
-  def walk_steps([], {x, y, z}) do
-    {x, y, z}
+  def walk_steps([], _, max) do
+    max
   end
-  def walk_steps([step | rest], {x, y, z}) do
+  def walk_steps([step | rest], {x, y, z}, max) do
     {mx, my, mz} = case step do
       "n" ->  { 0,  1, -1}
       "ne" -> { 1,  0, -1}
@@ -42,7 +44,10 @@ defmodule Puzzle do
       "sw" -> {-1,  0,  1}
       "se" -> { 1, -1,  0}
     end
-    walk_steps(rest, {x + mx, y + my, z + mz})
+    new_pos = {x + mx, y + my, z + mz}
+    steps = count_steps(new_pos, 0)
+    max = if max > steps, do: max, else: steps
+    walk_steps(rest, new_pos, max)
   end
 end
 
@@ -52,10 +57,10 @@ defmodule PuzzleTest do
   use ExUnit.Case
 
   test "case 1", do: assert Puzzle.call("ne,ne,ne") == 3
-  test "case 2", do: assert Puzzle.call("ne,ne,sw,sw") == 0
+  test "case 2", do: assert Puzzle.call("ne,ne,sw,sw") == 2
   test "case 3", do: assert Puzzle.call("ne,ne,s,s") == 2
   test "case 4", do: assert Puzzle.call("se,sw,se,sw,sw") == 3
-  test "case 5", do: assert Puzzle.call("n,se,s,sw,nw,n,ne,s") == 0
+  test "case 5", do: assert Puzzle.call("n,se,s,sw,nw,n,ne,s") == 1
 end
 
 File.read!("input-day11.txt")
